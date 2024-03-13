0992834e791fe7f55e16a35996ae8bff6bda22f8
MartuV <serranomv@hotmail.com>
2024-03-13 14:10:45 -0300

    gh pages agregado correctamente

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Prueba ghpages</h2>
+        <h2>Prueba ghpages
+        </h2>
         <a
           className="App-link"
           href="https://reactjs.org"
